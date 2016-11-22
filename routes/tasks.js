e8ef846af764476303d4fee11c6746f2ab90a588
Farhan Ali <e.alifarhan@gmail.com>
2016-11-22 23:16:22 +0530

    Debugging get tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -44,6 +44,7 @@ exports.readTasks = function(req, res){
     var userTasks = _.filter(state.tasks, { "userId": user.id });
 
     var status = req.query.active;
+    console.log(status);
     if (status) {
         userTasks = _.filter(userTasks, { "isActive": status });
     }
